e31c782beaf94e2fe5abeb338593f349c44c5206
chocoanish <chocoboyanish566@gmail.com>
2025-03-01 11:10:08 +0530

    refactor Features component to update video file paths and enhance styling

--- src/components/layoutComponent/Landing/Features.tsx
+++ src/components/layoutComponent/Landing/Features.tsx
@@ -117,7 +117,7 @@ const Features: React.FC = () => (
 
       <BentoTilt className={styles.bentoTiltContainer}>
         <BentoCard
-          src="videos/feature-1.mp4"
+          src="videos/feature/feature-1.mp4"
           title={<> radia<b>n</b>t </>}
           description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
           isComingSoon
@@ -127,7 +127,7 @@ const Features: React.FC = () => (
       <div className={styles.bentoGrid}>
         <BentoTilt className={styles.bentoTilt1}>
           <BentoCard
-            src="videos/feature-2.mp4"
+            src="videos/feature/feature-2.mp4"
             title={<> zig<b>m</b>a </>}
             description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
             isComingSoon
@@ -136,7 +136,7 @@ const Features: React.FC = () => (
 
         <BentoTilt className={styles.bentoTilt1}>
           <BentoCard
-            src="videos/feature-3.mp4"
+            src="videos/feature/feature-3.mp4"
             title={<> n<b>e</b>xus </>}
             description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
             isComingSoon
@@ -145,23 +145,23 @@ const Features: React.FC = () => (
 
         <BentoTilt className={styles.bentoTilt1}>
           <BentoCard
-            src="videos/feature-4.mp4"
+            src="videos/feature/feature-4.mp4"
             title={<> az<b>u</b>l </>}
             description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
             isComingSoon
           />
         </BentoTilt>
 
-        <BentoTilt className={styles.bentoTilt2}>
+        {/* <BentoTilt className={styles.bentoTilt2}>
           <div className={styles.moreComingSoonContainer}>
             <h1 className={styles.moreComingSoonText}>M<b>o</b>re co<b>m</b>ing s<b>o</b>on.</h1>
             <TiLocationArrow className={styles.largeIcon} />
           </div>
-        </BentoTilt>
+        </BentoTilt> */}
 
         <BentoTilt className={styles.bentoTilt2}>
           <video
-            src="videos/feature-5.mp4"
+            src="videos/feature/feature-5.mp4"
             loop
             muted
             autoPlay
