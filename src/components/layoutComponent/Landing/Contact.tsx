61b6ec0aaeeb4b8559adfb9c126ebcaaff3f71e8
chocoanish <chocoboyanish566@gmail.com>
2025-03-03 17:45:44 +0530

    add Contact component with new styles and interactive elements; integrate button for user interaction

--- src/components/layoutComponent/Landing/Contact.tsx
+++ src/components/layoutComponent/Landing/Contact.tsx
@@ -1,4 +1,5 @@
 import styles from "./Contact.module.css";
+import Button from "../../../ui/buttons/Button";
 
 const ImageClipBox = ({src, alt, className}: {src: string, alt: string, className: string}) => {
   return (
@@ -18,6 +19,30 @@ const Contact = () => {
             src="img/contact-1.webp"
             alt="" 
           />
+          <ImageClipBox
+            className={styles.contactClipPath2}
+            src="img/contact-2.webp"
+            alt="" 
+          />
+        </div>
+
+        <div className={styles.contact2}>
+            <ImageClipBox 
+                src="img/swordman-partial.webp"
+                alt="swordman"
+                className={styles.contactClipPath3}    
+            />
+            <ImageClipBox 
+                src="img/swordman.webp"
+                alt="swordman"
+                className={styles.contactClipPath4}    
+            />
+        </div>
+
+        <div className={styles.contactText}>
+            <p className={styles.contactTitle}>Join Gameflow Studio</p>
+            <p className={styles.contactDescription}>Let's build the new era of gaming together</p>
+            <Button title="contact us" containerClass={styles.contactButton}/>
         </div>
       </div>
     </div>
